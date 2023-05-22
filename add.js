09d77903aa30d1df88cdf245f68a73b7f921daf7
Vamsi1656 <vamsireddy1717@gmail.com>
2023-05-22 11:58:08 +0530

    first commit

---
+++ add.js
@@ -0,0 +1,22 @@
+let a=2;
+let b=3;
+let c='4';
+let d='nani';
+let e="sai";
+let f="pallavi";
+let g=true;
+let h=false;
+let i;
+
+console.log(a+b) // number and number
+console.log(a+c) // number and string
+console.log(a+d) // number and string name
+console.log(a+i) // number and undefined
+console.log(d+e) // string and string
+console.log(e+f)
+console.log(d+i) // string and undefined
+console.log(a+g) // number and boolean true here true is 1
+console.log(a+h) // number and boolean false here true is 0
+console.log(g+h) // boolean and boolean
+console.log(g+i) // boolean and undefined
+
---
+++ multiply.js
@@ -0,0 +1,20 @@
+let a=2;
+let b=3;
+let c='4';
+let d='nani';
+let e="sai";
+let f="pallavi";
+let g=true;
+let h=false;
+let i;
+
+console.log(a*b) // number and number
+console.log(a*c) // number and string
+console.log(a*d) // number and string name
+console.log(a*g) // number and boolean value true here true is one
+console.log(a*h) // number and boolean value false here false is zero
+console.log(a*i) // number and undefined
+console.log(e*f) // string and string
+console.log(g*h) // boolean and boolean
+console.log(d*i) // string and undefined
+console.log(g*i) // boolean and undefined
